cf94cbe8e5994d47b5a04addea85c4f9725df283
willianbello <willianbello.s@gmail.com>
2020-01-27 09:57:22 -0300

    Criando página de produtos

---
+++ src/pages/produtos/produtos.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ProdutoDTO } from '../../models/produto.dto';
+import { API_CONFIG } from '../../config/api.config';
+
+@IonicPage()
+@Component({
+  selector: 'page-produtos',
+  templateUrl: 'produtos.html',
+})
+export class ProdutosPage {
+
+  items: ProdutoDTO[];
+
+  bucketBaseUrl: string = API_CONFIG.bucketBaseUrl;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.items = [
+      {
+        id: '1',
+        nome: 'Mouse',
+        preco: 80.99
+      },
+      {
+        id: '2',
+        nome: 'Teclado',
+        preco: 80.99
+      }
+    ]
+  }
+
+}
